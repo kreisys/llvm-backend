0ac5ce0b67760b3eff8e5b350e37fa4bf051230b
Shay Bergmann <shay.bergmann@iohk.io>
2021-09-17 23:50:17 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -1,14 +1,58 @@
 {
   description = "llvm-backend flake️";
 
-  inputs.utils.url = "github:kreisys/flake-utils";
+  inputs = {
+    utils.url = "github:kreisys/flake-utils";
+    mavenix.url = "github:nix-community/mavenix/3ac30863abb9a8986560ed4dfa5e6288434b73e1";
+  };
 
   outputs = { self, nixpkgs, utils }: utils.lib.simpleFlake {
     inherit nixpkgs;
     systems = [ "x86_64-darwin" "x86_64-linux" ];
 
-    packages = { system }: rec {
-      inherit (import self { inherit system; }) llvm-backend;
+    overlay = final: prev: with final; {
+      llvmPackages = prev.llvmPackages_10;
+      # The backend requires clang/lld/libstdc++ at runtime.
+      # The closest configuration in Nixpkgs is clang/lld without any C++ standard
+      # library. We override that configuration to inherit libstdc++ from stdenv.
+      clang =
+        let
+          override = attrs: {
+            extraBuildCommands = ''
+                ${attrs.extraBuildCommands}
+              sed -i $out/nix-support/cc-cflags -e '/^-nostdlib/ d'
+            '';
+          };
+        in
+        llvmPackages.lldClangNoLibcxx.override override;
+
+      llvm-backend = callPackage ./nix/llvm-backend.nix {
+        release = false;
+        host.clang = clang;
+      };
+
+      mavenix = callPackage mavenix {};
+
+      llvm-backend-matching = callPackage ./nix/llvm-backend-matching.nix {};
+
+      llvm-kompile-testing =
+        let
+          java = "${jre}/bin/java";
+          inherit (llvm-backend-matching) jar;
+        in
+        runCommandNoCC "llvm-kompile-testing" { } ''
+          mkdir -p "$out/bin"
+          cp ${llvm-backend.src}/bin/llvm-kompile-testing "$out/bin"
+          sed -i "$out/bin/llvm-kompile-testing" \
+          -e '/@PROJECT_SOURCE_DIR@/ c ${java} -jar ${jar} $definition qbaL $dt_dir 1'
+          chmod +x "$out/bin/llvm-kompile-testing"
+          patchShebangs "$out/bin/llvm-kompile-testing"
+        '';
+
+    };
+
+    packages = { llvm-backend }: rec {
+      inherit llvm-backend;
       defaultPackage = llvm-backend;
     };
   };
